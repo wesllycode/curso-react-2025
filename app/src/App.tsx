01a9aa4b9f5c6b4f28ebfcf5352c08723cdcb438
wesllycode <wesllycode07@gmail.com>
2025-07-23 15:40:36 -0300

    feat: aula 2 concluída
    Signed-off-by: wesllycode <wesllycode07@gmail.com>

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
+import {useState} from "react";
 
 
 function App() {
-
+const [count, setCount] = useState(0)
 
   return (
       <>
+          <p>Hello World</p>
+          <button onClick={() => setCount(count + 1)}>
+              Count {count}
+          </button>
       </>
   )
 }
