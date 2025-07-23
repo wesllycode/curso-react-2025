0b0fc8e67064e3bebe4c8fe882599c416f7cf25d
wesllycode <wesllycode07@gmail.com>
2025-07-23 14:23:33 -0300

    feat: configure Vite server settings for external connections
    Signed-off-by: wesllycode <wesllycode07@gmail.com>

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Permite conexões externas
+    port: 5173,      // Porta que será utilizada
+    strictPort: true // Garante uso dessa porta
+  },
   plugins: [react()],
+
 })
