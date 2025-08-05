01bfdb9059e4bb48392ef9f53830c777b769f400
wesllycode <wesllycode07@gmail.com>
2025-08-05 15:00:22 -0300

    feat: atualizar componente App para exibir mensagem em português e simplificar estrutura

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,16 @@
-import {useState} from "react";
-
+import { useState } from "react"
 
 function App() {
-const [count, setCount] = useState(0)
+
+    const [count, setCount] = useState(0)
 
   return (
-      <div className="app-container">
-          <p>Hello World</p>
-          <div className="button-container">
-            <button onClick={() => setCount(count + 1)}>
-                Count {count}
-            </button>
-          </div>
-         
-      </div>
+      <>       
+        <p> Meu primeiro projeto em react</p>     
+        <button onClick={() => setCount(count + 1)}>
+            Count {count} 
+        </button>
+      </>
   )
 }
 
