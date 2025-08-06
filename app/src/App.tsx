866eeacf794755fb0fcda265a287a58f5ef5c41a
wesllycode <wesllycode07@gmail.com>
2025-08-05 21:07:45 -0300

    feat: adicionar componente Test ao App para exibir mensagem em português

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,18 @@
+
+const Test = () => {
+    return (
+      <div>Aqui é componente um React</div>
+    )
+}
+
 export function App() {  
 
   return (
       <>       
        Tudo vai começar
+       <Test />
       </>
   )
+  
+  
 }
